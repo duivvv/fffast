d07466e4bbd84c2e1a98aa69e896865a2874c6ca
duivvv <geoffrey.dhuyvetters@gmail.com>
2015-12-16 15:23:56 +0100

    fixed path error

--- src/lib/loaders/babel.js
+++ src/lib/loaders/babel.js
@@ -4,7 +4,7 @@ module.exports = function(modulePath, remotePath){
 
   var arr = ['babel-preset-es2015', 'babel-preset-stage-0', 'babel-preset-react'];
   arr = arr.map(function(preset){
-    return path.relative(remotePath, modulePath + '/' + preset);
+    return path.relative(path.resolve(remotePath, '..'), modulePath + '/' + preset);
   });
 
   return {
